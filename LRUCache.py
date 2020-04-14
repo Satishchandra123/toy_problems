03e503e8ffb57d4ef944d6a0657977b5e28f65c8
Satishchandra123 <chundrusatish8@msitprogram.net>
2020-04-14 13:05:47 +0530

    final

--- LRUCache.py
+++ LRUCache.py
@@ -1,13 +1,27 @@
 class LRUCache:
-    def init(self,capacity):
+
+    def __init__(self,capacity):
         super().__init__()
         self.capacity = capacity
         self.cache = {}
         self.lru = {}
+
     def put(self,key,value):
-        pass
+        if key not in self.cache and len(self.cache) == self.capacity:
+            old_key = min(self.lru.keys(), key = lambda k:self.lru[k])
+            self.cache.pop(old_key)
+            self.lru.pop(old_key)
+        self.cache[key] = value
+        self.lru[key] = 1
+        return "put success"
+
     def get(self,key):
-        pass
+        if key in self.cache:
+            self.lru[key] += 1
+            return self.cache[key]
+        else:
+            print("Not there in cache")
+
     def get_cache(self):
-        pass
+        return self.cache
 
